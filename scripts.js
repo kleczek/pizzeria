711dfa5b1a7540f04cface664415043b0efa22fb
EK <ek.studio.web@gmail.com>
2019-10-18 01:38:20 +0200

    added media queries to main page

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 //doesn't work  start
-$('.menu') .on('click', function(){
-	$(this) .toggleClass('active');
+$('.menu').on('click', function () {
+	$(this).toggleClass('active');
 	//doesn't work end
-	&('.overlay').toggleClass('menu-open');
+	$('.overlay').toggleClass('menu-open');
 });
 
 $('.nav a').on('click', function(){
 	$('.menu').removeClass('active');
 	$('.overlay').removeClass('menu-open');
-})
+});
